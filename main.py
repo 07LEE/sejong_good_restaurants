7fc02fda749ce324220aa93352ffc080fb10c65a
Lee <yzz0728@gmail.com>
2023-05-22 16:03:05 +0900

    Add data processing process

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-#%%
 import pandas as pd
 import numpy as np
-import os
+import os, re
 import category_kakao as kakao
 import prepro_data as ppdata
 
@@ -14,24 +13,31 @@ df = ppdata.preprocess_excel_files('prepro_data')
 df = ppdata.reset_and_drop_index(df)
 df = ppdata.rename_and_reindex_columns(df)
 
-remove_list = ['조치원점', '조치원지점', '대평점', '전의점', '세종점', '보람점', '본점', '반곡점', '새롬점', '시청점', '시청스카이점', '나성점', '신흥점', '\(주\)', '\(세종\)', ' 세종', '\(LeBeef\)', '\(stay\)', '\(\)', '㈜신화케이푸드', ' ', '외1건','외1개소', '외1']
+remove_list = ['조치원점', '조치원지점', '대평점', '전의점', '세종점', '보람점', '본점', '반곡점', '새롬점', '시청점', '시청스카이점', '나성점', '신흥점', '(주)', '(세종)', ' 세종', '(LeBeef)', '(stay)', r'\(\)', '㈜신화케이푸드', ' ', '외1건','외1개소', '외1']
 
 for i in remove_list:
     ppdata.re_rename(df, 'name', i, '')
 
 ppdata.re_rename(df, 'name', '그릴200도씨', '그릴200도')
 ppdata.re_rename(df, 'name', '그릴 200℃', '그릴200도')
-df = df[df["name"].notnull()]
 
-kakao.get_category(df, API_KEY)
+df = ppdata.drop_null(df)
+ppdata.drop_cost_comma(df)
 
+try:
+    kakao.get_category(df, API_KEY)
+except:
+    print('can not get category. check API')
 
-# %% 파일 저장
-# df.to_csv('df.csv', encoding='UTF-8')
-# df.to_excel("df_with_category.xlsx")
+df['name'] = df['name'].replace('-', '')
+df['people'] = df['people'].replace('다수', '').replace('-', '')
+df = df[df['name'] != '']
+df = df[df['people'] != '']
+df['people'] = df['people'].astype(int)
+
+# 파일 저장
 if not os.path.exists('output_data'):
     os.makedirs('output_data')
 
-df.to_excel('output_data\\df.xlsx')
-
-# %%
+df.to_csv('output_data\\data.csv', encoding='UTF-8')
+df.to_json('output_data\\data.json', orient='records', indent=4, force_ascii=False)
--- prepro_data.py
+++ prepro_data.py
@@ -21,19 +21,21 @@ def preprocess_excel_files(excel_files_dir):
 
   for excel_file_name in os.listdir(excel_files_dir):
     if excel_file_name.endswith('.xlsx'):
-      df = pd.read_excel(os.path.join(excel_files_dir, excel_file_name), skiprows=3, usecols=[4, 5, 6], header=None)
+      df = pd.read_excel(os.path.join(
+          excel_files_dir, excel_file_name), skiprows=3, usecols=[4, 5, 6], header=None)
       df['department'] = excel_file_name[:-5]
-      df['date'] = '2023.03'
+      df['date'] = '2023-03-01'
       df_list.append(df)
 
   df = pd.concat(df_list)
-
   return df
+
 
 def reset_and_drop_index(df):
   df = df.reset_index()
   df = df.drop('index', axis=1)
   return df
+
 
 def rename_and_reindex_columns(df):
   df = df.rename(columns={4: 'cost', 5: 'name', 6: 'people'})
@@ -42,5 +44,18 @@ def rename_and_reindex_columns(df):
   df['name'] = df['name'].str.split('/').str[0]
   return df
 
+
 def re_rename(df, a, word, rename):
-    df[a] = df[a].str.replace(word, rename)
+  df[a] = df[a].str.replace(word, rename, regex=False)
+
+
+def drop_cost_comma(df):
+  df['cost'] = df['cost'].astype(str)
+  df['cost'] = df['cost'].str.replace(',', '').str.replace('[^0-9]', '', regex=False)
+  df['cost'] = df['cost'].astype(int)
+
+
+def drop_null(df):
+  for i in df.columns:
+    df = df[df[i].notnull()]
+  return df
